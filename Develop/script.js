41867a0e60a4e8faf655fc1d718536bedaa211f7
BrienB <barrdom@hotmail.com>
2020-11-18 00:33:35 -0500

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,110 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Function to generate the password
+function generatePassword() {
+
+  // Declare the new password variable
+  var newPassword = "";
+
+  // User input via confirms and prompts
+  var confirm1 = confirm("Do you want to generate a new password?");
+
+  // If user wants to create a new password
+  if (confirm1 === true){
+    // Continue user input via prompts and confirms
+    var prompt2 = prompt("How many characters do you want your password to contain? \n Please pick from 8-128.");
+      
+    // Create variable of posible characters
+    var possibleCharacters = [
+      ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+      ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+      ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+      [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", ",", "{", "|", "}", "~"],
+    ];
+
+      // The number of characters that the user chooses parsed to an integer
+      var characters = parseInt(prompt2);
+      
+      // If the number of characters the user chooses is within the range of 8-128
+      if (characters >= 8 && characters <= 128 && prompt2 !== ""){
+        // Continue user input via prompts and confirms
+        var confirm3 = confirm("Do you want your password to contain numbers?");
+        var confirm4 = confirm("Do you want your password to contain lower case letters?");
+        var confirm5 = confirm("Do you want your password to contain upper case letters?");
+        var confirm6 = confirm("Do you want your password to contain special characters?");
+        // For loop to pick characters
+        for (i = 0; i < characters; i++){
+          // Generate a random index number
+          var charIndex = Math.floor(Math.random() * possibleCharacters.length);
+          
+          // If the user wants to include numbers, generate a number
+          if (confirm3 === true && charIndex === 0){
+            // Generate a random index of the numbers array
+            var numIndex = Math.floor(Math.random() * possibleCharacters[0].length);
+            // Store the new character
+            var newChar = possibleCharacters[0][numIndex];
+          }
+
+          // If the user wants to include lower case letters, generate a lower case letter
+          else if (confirm4 === true && charIndex === 1){
+            // Generate a random index of the lowerLetters array
+            var lowerIndex = Math.floor(Math.random() * possibleCharacters[1].length);
+            // Store the new character
+            var newChar = possibleCharacters[1][lowerIndex];
+          }
+
+          // If the user wants to include upper case letters generate an upper case letter
+          else if (confirm5 === true && charIndex === 2){
+            // Generate a random index of the upperLetters array
+            var upperIndex = Math.floor(Math.random() * possibleCharacters[2].length);
+            // Store the new character
+            var newChar = possibleCharacters[2][upperIndex];
+          }
+
+          // If the user wants to a special character generate a special character
+          else if (confirm5 === true && charIndex === 3){
+            // Generate a random index of the specialChars array
+            var specIndex = Math.floor(Math.random() * possibleCharacters[3].length);
+            // Store the new character
+            var newChar = possibleCharacters[3][specIndex];
+          }
+          else{
+            i = i-1;
+            newChar = "";
+          }
+
+          //Add the new character to the new password
+          newPassword = newPassword + newChar;
+          
+        //End of for loop
+        }
+        
+        return(newPassword);
+      
+      // End of if statement
+      }
+      else{
+        newPassword = "Your Secure Password";
+      }
+  }
+  else{
+    newPassword = "Your Secure Password";
+    document.querySelector("#password").value = newPassword;
+    return(newPassword);
+  }
+
+// End of function
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
---
+++ README.md
@@ -0,0 +1,8 @@
+# Password Generator
+
+## My Task
+
+In this homework assignment I was asked to create an application that an employee can use to generate a random password based on criteria they’ve selected by modifying starter code. This app runs in the browser, and features dynamically updated HTML and CSS powered by JavaScript code that I wrote. I was tasked to give it a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
+
+I was also tasked to make it possible for the password to include special characters.
+
