99317559b3d48edb5b0c115867a853a6751eb748
BrienB <barrdom@hotmail.com>
2020-11-20 00:23:55 -0500

    updated javascript to clear the newPassword variable before generating a new password

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Declare the password textarea text value as a variable
+var passwordText = document.querySelector("#password");
+
 // Declare the new password variable
 var newPassword = "";
+
+// Create variable of posible characters
+var possibleCharacters = [
+  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", ",", "{", "|", "}", "~"],
+];
 
 ///////////////////////////////////////////////////////////////
 
 // Function to generate the password
-function generatePassword() {
-
-  // Declare the password textarea text value as a variable
-  var passwordText = document.querySelector("#password");
+function generatePassword() { 
+  
+  
 
   // If the password text area contains a password, store the value to a variable in case 
   // the user accidentally clicks the generate password button
-  if (passwordText.value === newPassword)
-  var oldPassword = passwordText.value;
-
+  if (passwordText.value === newPassword){    
+    var oldPassword = passwordText.value;
+    console.log(oldPassword);
+  }
   // User input via confirms and prompts
   var confirm1 = confirm("Do you want to generate a new password?");
 
@@ -26,19 +37,14 @@ function generatePassword() {
     // Continue user input via prompts and confirms
     var prompt2 = prompt("How many characters do you want your password to contain? \n Please pick from 8-128.");
 
-    // Create variable of posible characters
-    var possibleCharacters = [
-      ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-      ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-      ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-      [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", ",", "{", "|", "}", "~"],
-    ];
-
     // The number of characters that the user chooses parsed to an integer
     var characters = parseInt(prompt2);
 
     // If the number of characters the user chooses is within the range of 8-128
     if (characters >= 8 && characters <= 128 && prompt2 !== ""){
+
+      // Clear the newPassword variable
+      newPassword = "";
 
       // Continue user input via prompts and confirms
       var confirm3 = confirm("Do you want your password to contain numbers?");
@@ -110,7 +116,7 @@ function generatePassword() {
     // If the number of characters entered in the prompt is out of the range
     else if(characters < 8 || characters > 128){
       passwordText.value = "The number of characters you chose for your password is outside of the range.\n\Click the button below to generate a new password.";
-      return(passwordText.value);
+      // return(passwordText.value);
     }
 
     // If the user cancels the password generator when prompted to enter the number of characters
@@ -124,8 +130,9 @@ function generatePassword() {
 
   // If the user cancels the password generator when asked to confirm creation of the password
   else{
+    console.log(newPassword);
     // newPassword = "Click the button below to generate a new password.";
-    passwordText.value = newPassword;
+    passwordText.value = oldPassword;
     return(newPassword);
   }
 
